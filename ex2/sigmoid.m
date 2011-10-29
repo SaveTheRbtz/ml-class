53da6d0433fc7a35fc8f1551d6d790f56991f073
Alexey Ivanov <SaveTheRbtz@GMail.com>
2011-10-29 05:04:49 +0400

    [ex2][sigmoid] Finished warmup excersise.
    Pretty streight-forward. Only trick is to use per element / and ^

--- ex2/sigmoid.m
+++ ex2/sigmoid.m
@@ -8,10 +8,7 @@ g = zeros(size(z));
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
-
-
-
-
+g = 1 ./ (1 + e .^ -z);
 
 % =============================================================
 
