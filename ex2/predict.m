12553dd82dd9004f28129308d4e9d23dcc26a0f2
Alexey Ivanov <SaveTheRbtz@GMail.com>
2011-10-29 05:49:00 +0400

    [ex2][predict] Simply round sigmoid(h0(x)) to the nearest int.
    Do not have input validation but pretty fast.
    
    TODO(SaveTheRbtz@): Maybe it's reasonable to move sigmoid(X*theta) to separate
    function

--- ex2/predict.m
+++ ex2/predict.m
@@ -15,12 +15,7 @@ p = zeros(m, 1);
 %               You should set p to a vector of 0's and 1's
 %
 
-
-
-
-
-
-
+p = round(sigmoid(X*theta));
 % =========================================================================
 
 
