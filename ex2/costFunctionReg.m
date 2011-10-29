4b1d40bda058064dc549d731a6bb3ee8ae75d062
Alexey Ivanov <SaveTheRbtz@GMail.com>
2011-10-30 02:27:38 +0400

    [ex2][costFunctionReg] Use costFunction and penalize it.

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -17,11 +17,11 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+[J, grad] = costFunction(theta, X, y);
+penalize = sum(theta(2:length(theta)) .^ 2);
+J = J + lambda/(2*m) * penalize;
 
-
-
-
-
+grad(2:length(grad)) = grad(2:length(grad)) + (lambda/m)*theta(2:length(theta));
 % =============================================================
 
 end
