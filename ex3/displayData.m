2322e38a4ee942f8a2ac905ff3f920c72595c515
Alexey Ivanov <SaveTheRbtz@GMail.com>
2011-11-04 21:27:40 +0400

    [ex3] Arrrr! -tabs; -trailing spaces;

--- ex3/displayData.m
+++ ex3/displayData.m
@@ -1,12 +1,12 @@
 function [h, display_array] = displayData(X, example_width)
 %DISPLAYDATA Display 2D data in a nice grid
 %   [h, display_array] = DISPLAYDATA(X, example_width) displays 2D data
-%   stored in X in a nice grid. It returns the figure handle h and the 
+%   stored in X in a nice grid. It returns the figure handle h and the
 %   displayed array if requested.
 
 % Set example_width automatically if not passed in
-if ~exist('example_width', 'var') || isempty(example_width) 
-	example_width = round(sqrt(size(X, 2)));
+if ~exist('example_width', 'var') || isempty(example_width)
+    example_width = round(sqrt(size(X, 2)));
 end
 
 % Gray Image
@@ -30,22 +30,22 @@ display_array = - ones(pad + display_rows * (example_height + pad), ...
 % Copy each example into a patch on the display array
 curr_ex = 1;
 for j = 1:display_rows
-	for i = 1:display_cols
-		if curr_ex > m, 
-			break; 
-		end
-		% Copy the patch
-		
-		% Get the max value of the patch
-		max_val = max(abs(X(curr_ex, :)));
-		display_array(pad + (j - 1) * (example_height + pad) + (1:example_height), ...
-		              pad + (i - 1) * (example_width + pad) + (1:example_width)) = ...
-						reshape(X(curr_ex, :), example_height, example_width) / max_val;
-		curr_ex = curr_ex + 1;
-	end
-	if curr_ex > m, 
-		break; 
-	end
+    for i = 1:display_cols
+        if curr_ex > m,
+            break;
+        end
+        % Copy the patch
+
+        % Get the max value of the patch
+        max_val = max(abs(X(curr_ex, :)));
+        display_array(pad + (j - 1) * (example_height + pad) + (1:example_height), ...
+                      pad + (i - 1) * (example_width + pad) + (1:example_width)) = ...
+                        reshape(X(curr_ex, :), example_height, example_width) / max_val;
+        curr_ex = curr_ex + 1;
+    end
+    if curr_ex > m,
+        break;
+    end
 end
 
 % Display Image
