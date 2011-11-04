2490c85cbea8e1c50e873c7843765019a216b8e0
Alexey Ivanov <SaveTheRbtz@GMail.com>
2011-11-04 23:08:55 +0400

    [ex3][fmincg] Fixed octave warning
    Octave produces warning: possible Matlab-style short-circuit operator at line.
    
    Replaced | and & with || and && respectfully in fmincg.m
    Hope didn't break anithing.
    
    PS. Do not edit code that you don't fully understand =)

--- ex3/fmincg.m
+++ ex3/fmincg.m
@@ -92,7 +92,7 @@ while i < abs(length)                                      % while not finished
   if length>0, M = MAX; else M = min(MAX, -length-i); end
   success = 0; limit = -1;                     % initialize quanteties
   while 1
-    while ((f2 > f1+z1*RHO*d1) | (d2 > -SIG*d1)) & (M > 0) 
+    while ((f2 > f1+z1*RHO*d1) || (d2 > -SIG*d1)) && (M > 0) 
       limit = z1;                                         % tighten the bracket
       if f2 > f1
         z2 = z3 - (0.5*d3*z3*z3)/(d3*z3+f2-f3);                 % quadratic fit
@@ -101,7 +101,7 @@ while i < abs(length)                                      % while not finished
         B = 3*(f3-f2)-z3*(d3+2*d2);
         z2 = (sqrt(B*B-A*d2*z3*z3)-B)/A;       % numerical error possible - ok!
       end
-      if isnan(z2) | isinf(z2)
+      if isnan(z2) || isinf(z2)
         z2 = z3/2;                  % if we had a numerical problem then bisect
       end
       z2 = max(min(z2, INT*z3),(1-INT)*z3);  % don't accept too close to limits
@@ -112,7 +112,7 @@ while i < abs(length)                                      % while not finished
       d2 = df2'*s;
       z3 = z3-z2;                    % z3 is now relative to the location of z2
     end
-    if f2 > f1+z1*RHO*d1 | d2 > -SIG*d1
+    if f2 > f1+z1*RHO*d1 || d2 > -SIG*d1
       break;                                                % this is a failure
     elseif d2 > SIG*d1
       success = 1; break;                                             % success
@@ -122,19 +122,19 @@ while i < abs(length)                                      % while not finished
     A = 6*(f2-f3)/z3+3*(d2+d3);                      % make cubic extrapolation
     B = 3*(f3-f2)-z3*(d3+2*d2);
     z2 = -d2*z3*z3/(B+sqrt(B*B-A*d2*z3*z3));        % num. error possible - ok!
-    if ~isreal(z2) | isnan(z2) | isinf(z2) | z2 < 0   % num prob or wrong sign?
+    if ~isreal(z2) || isnan(z2) || isinf(z2) || z2 < 0   % num prob or wrong sign?
       if limit < -0.5                               % if we have no upper limit
         z2 = z1 * (EXT-1);                 % the extrapolate the maximum amount
       else
         z2 = (limit-z1)/2;                                   % otherwise bisect
       end
-    elseif (limit > -0.5) & (z2+z1 > limit)          % extraplation beyond max?
+    elseif (limit > -0.5) && (z2+z1 > limit)          % extraplation beyond max?
       z2 = (limit-z1)/2;                                               % bisect
-    elseif (limit < -0.5) & (z2+z1 > z1*EXT)       % extrapolation beyond limit
+    elseif (limit < -0.5) && (z2+z1 > z1*EXT)       % extrapolation beyond limit
       z2 = z1*(EXT-1.0);                           % set to extrapolation limit
     elseif z2 < -z3*INT
       z2 = -z3*INT;
-    elseif (limit > -0.5) & (z2 < (limit-z1)*(1.0-INT))   % too close to limit?
+    elseif (limit > -0.5) && (z2 < (limit-z1)*(1.0-INT))   % too close to limit?
       z2 = (limit-z1)*(1.0-INT);
     end
     f3 = f2; d3 = d2; z3 = -z2;                  % set point 3 equal to point 2
@@ -159,7 +159,7 @@ while i < abs(length)                                      % while not finished
     ls_failed = 0;                              % this line search did not fail
   else
     X = X0; f1 = f0; df1 = df0;  % restore point from before failed line search
-    if ls_failed | i > abs(length)          % line search failed twice in a row
+    if ls_failed || i > abs(length)          % line search failed twice in a row
       break;                             % or we ran out of time, so we give up
     end
     tmp = df1; df1 = df2; df2 = tmp;                         % swap derivatives
