8d57a32f182de6749d8d532d275b53788a0fdb6a
Alexey Ivanov <SaveTheRbtz@GMail.com>
2011-11-04 23:11:32 +0400

    [ex3][oneVsAll] Not vectorized implementation.

--- ex3/oneVsAll.m
+++ ex3/oneVsAll.m
@@ -49,16 +49,11 @@ X = [ones(m, 1) X];
 %                 initial_theta, options);
 %
 
+for c = 1:num_labels
+    options = optimset('GradObj', 'on', 'MaxIter', 50);
 
-
-
-
-
-
-
-
-
-
+    all_theta(c, :) = fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), ...
+            zeros(n + 1, 1), options);
 
 % =========================================================================
 
