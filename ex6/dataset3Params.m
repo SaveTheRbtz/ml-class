5137f8261f36d105d149a6319a79a8f1a62ae773
Alexey Ivanov <SaveTheRbtz@GMail.com>
2011-11-27 03:41:33 +0400

    [ex6] fixed comments

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -1,8 +1,8 @@
 function [C, sigma] = dataset3Params(X, y, Xval, yval)
-%EX6PARAMS returns your choice of C and sigma for Part 3 of the exercise
+%DATASET3PARAMS returns your choice of C and sigma for Part 3 of the exercise
 %where you select the optimal (C, sigma) learning parameters to use for SVM
 %with RBF kernel
-%   [C, sigma] = EX6PARAMS(X, y, Xval, yval) returns your choice of C and 
+%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and 
 %   sigma. You should complete this function to return the optimal C and 
 %   sigma based on a cross-validation set.
 %
