5a6c6f1f2f1dd76c7a1d87b505c50b84acde8abc
Alexey Ivanov <SaveTheRbtz@GMail.com>
2011-12-05 09:23:27 +0400

    [ex7] Recover Data (PCA)

--- ex7/recoverData.m
+++ ex7/recoverData.m
@@ -21,7 +21,13 @@ X_rec = zeros(size(Z, 1), size(U, 1));
 %               Notice that U(j, 1:K) is a row vector.
 %               
 
-
+% TODO(SaveTheRbtz@): Vectorize me!
+for i = 1:size(Z, 1)
+    v = Z(i, :)';
+    for j = 1:size(U, 1)
+        X_rec(i, j) = v' * U(j, 1:K)';
+    endfor
+endfor
 
 % =============================================================
 
